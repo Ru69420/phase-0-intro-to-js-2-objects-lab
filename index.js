67d724ba7c017b8e072c2ca28976aaf66ba9555c
RuthTadesse <tib6297@gmail.com>
2021-12-30 14:19:04 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+{
+    
+const employee = [employee, key, value] = args
+updateEmployeeWithKeyAndValue(Object,key ,value);
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee , Object);
+deleteFromEmployeeByKey(employee , Object , key);
+destructivelyDeleteFromEmployeeByKey(employee , Object);
+}
